5d64c573a616b88d920e1fd41366d8e45004c938
築山航平 <tsukiyama@mba.local>
2020-11-25 01:04:24 +0900

    section 10 11 done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,36 @@
 <template>
-    <p>Home</p>
+    <div>
+        <p v-border:solid.round.shadow="{width: '5px', color: 'red'}">Home</p>
+        <h2>{{ title | upperCase }}</h2>
+        <h3>{{ subTitle | upperCase }}</h3>
+        <p>{{ number }}</p>
+        <button @click="number++">+1</button>
+        <CountNumber></CountNumber>
+    </div>
 </template>
+
+<script>
+import CountNumber from "./CountNumber.vue"
+import { tokyoNumber } from "@/tokyoNumber.js";
+
+export default {
+    mixins: [tokyoNumber],
+    components: {
+        CountNumber, 
+    },
+    directives: {
+        border(el, binding) {
+            el.style.border = "solid black 2px";
+            el.style.borderWidth = binding.value.width;
+            el.style.borderColor = binding.value.color;
+            el.style.borderStyle = binding.arg;
+            if (binding.modifiers.round) {
+                el.style.borderRadius = "1.5rem";
+            }
+            if (binding.modifiers.shadow) {
+                el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 12.66)"
+            }
+        }
+    }
+}
+</script>
