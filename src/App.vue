076461fe1aa54d28ca838990ef53c793fb6f3dac
築山航平 <tsukiyama@mba.local>
2020-11-25 04:31:54 +0900

    section12 done

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,159 @@
 <template>
-    <div>
-        <LikeHeader header-text="hello">
-            <h3>はじめまして</h3>
-        </LikeHeader>
-        <LikeNumber :totalNumber = "number" @my-click="incrementNumber"></LikeNumber>
-        <button @click="currentComponent = 'Home'">Home</button>
-        <button @click="currentComponent = 'About'">About</button>
-        <keep-alive>
-            <component :is="currentComponent"></component>
-        </keep-alive>
-        <div style="padding: 10rem">
-            <h2>イベントのフォーム</h2>
-            <EventTitle v-model="eventData.title"></EventTitle>
-            <label for="detail">イベントの内容</label>
-            <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
-            <p style="white-space: pre">{{ eventData.detail }}</p>
-            <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
-            <label for="isPrivate">非公開</label>
-            <p>{{ eventData.isPrivate }}</p>
-            <p>参加条件</p>
-            <input type="checkbox" id="10" value="10代" v-model="eventData.target">
-            <label for="10">10代</label>
-            <input type="checkbox" id="20" value="20代" v-model="eventData.target">
-            <label for="20">20代</label>
-            <input type="checkbox" id="30" value="30代" v-model="eventData.target">
-            <label for="30">30代</label>
-            <p>{{ eventData.target }}</p>
-            <p>参加費</p>
-            <input type="radio" id="free" value="無料" v-model="eventData.price">
-            <label for="free">無料</label>
-            <input type="radio" id="paid" value="有料" v-model="eventData.price">
-            <label for="paid">有料</label>
-            <p>開催場所</p>
-            <select v-model="eventData.location" multiple>
-                <option v-for="location in locations" :key="location">{{ location }}</option>
-            </select>
-            <p>{{ eventData.location }}</p>
-        </div>
+    <div class="main">
+        <button @click = "myAnimation = 'slide'">Slide</button>
+        <button @click = "myAnimation = 'fade'">Fade</button>
+        <p>{{  myAnimation }}</p>
+        <br>
+        <button @click="add">追加</button>
+        <ul style="width: 200px; margin: auto;">
+            <transition-group name="fade" tag="div">
+                <li
+                    style="cursor: pointer;"
+                    v-for="(number, index) in numbers"
+                    :key="number"
+                    @click="remove(index)"
+                >
+                    {{ number }}
+                </li>
+            </transition-group>
+        </ul>
+        <br>
+        <button @click="show = !show">切り替え</button>
+        <br><br>
+        <transition
+            :css = "false"
+            @before-enter = "beforeEnter"
+            @enter = "enter"
+            @leave = "leave"
+        >
+            <div class="circle" v-if="show"></div>
+        </transition>
+        <button @click = "myComponent = 'ComponentA'">ComponentA</button>
+        <button @click = "myComponent = 'ComponentB'">ComponentB</button>
+        <transition name="fade" mode="out-in">
+            <component :is="myComponent"></component>
+        </transition>
     </div>
 </template>
 
 <script>
-import LikeHeader from './components/LikeHeader.vue';
-import About from './components/About.vue';
-import Home from './components/Home.vue';
-import EventTitle from './components/EventTitle.vue';
+import ComponentA from './components/ComponentA.vue';
+import ComponentB from './components/ComponentB.vue';
 
 export default {
+    components: {
+        ComponentA,
+        ComponentB,
+    },
     data() {
         return {
-            number: 14,
-            currentComponent: "Home",
-            locations: ['東京', '大阪', '名古屋'],
-            eventData: {
-                title: 'タイトル',
-                maxNumber: 0,
-                detail: "",
-                isPrivate: false,
-                target: [],
-                price: "無料",
-                location: [],
-            }
+            numbers: [0, 1, 2],
+            nextNumber: 3,
+            show: true,
+            myAnimation: 'slide',
+            myComponent: 'ComponentA',
         }
-    },
-    components: {
-        LikeHeader,
-        Home,
-        About,
-        EventTitle,
     },
     methods: {
-        incrementNumber(value) {
-            this.number = value;
-        }
+        randomIndex() {
+            return Math.floor(Math.random() * this.numbers.length)
+        },
+        add() {
+            this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+            this.nextNumber++;
+        },
+        remove(index) {
+            this.numbers.splice(index, 1);
+        },
+        beforeEnter(el) {
+            el.style.transform = 'scale(0)'
+        },
+        enter(el, done) {
+            let scale = 0;
+            const interval = setInterval(() => {
+                el.style.transform = `scale(${scale})`;
+                scale += .1;
+                if (scale > 1) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 2);
+        },
+        leave(el, done) {
+            let scale = 1;
+            const interval = setInterval(() => {
+                el.style.transform = `scale(${scale})`;
+                scale -= .1;
+                if (scale < 0) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 2);
+        },
     }
 }
 </script>
+
+<style scoped>
+.circle {
+    width: 200px;
+    height: 200px;
+    margin: 5rem auto;
+    background-color: deeppink;
+    border-radius: 50%;
+}
+
+.fade-move {
+    transition: transform 1s ;
+}
+.fade-enter {
+    opacity: 0;
+}
+.fade-enter-active {
+    transition: opacity 1s;
+}
+.fade-enter-to {
+    opacity: 1;
+}
+.fade-leave {
+    opacity: 1;
+}
+.fade-leave-active {
+    transition: opacity 1s;
+    position: absolute;
+    width: 200px;
+}
+.fade-leave-to {
+    opacity: 0;
+}
+
+.slide-enter,
+.slide-leave-to {
+    opacity: 0;
+}
+
+.slide-enter-active {
+    animation: slide-in .5s;
+    transition: opacity .5s;
+}
+.slide-leave-active {
+    animation: slide-in .5s reverse;
+    transition: opacity .5s;
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateX(100px);
+    }
+    to {
+        transform: translateX(0);
+    }
+}
+
+.main {
+    width: 70%;
+    margin: auto;
+    padding-top: 5rem;
+    text-align: center
+}
+</style>
